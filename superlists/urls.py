891b74be15479b7f1a98ed62183efb1499359697
Handymax <qicai21@gmail.com>
2021-03-28 01:22:03 +0800

    创建了地一个单元测试,添加了url_mapping, 添加了一个假的home_page view

--- superlists/urls.py
+++ superlists/urls.py
@@ -12,10 +12,19 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+
+管理后台暂时用不到
+    如果需要启用:
+    from django.contrib import admin
+
+    并在urlpatterns中添加:
+    url(r'^admin/', admin.site.urls),
+
 """
 from django.conf.urls import url
-from django.contrib import admin
+from lists import views
+
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home_page, name='home'),
 ]
