5c90a86b54c9d569433e0cf6d16e03cc1721bc77
Handymax <qicai21@gmail.com>
2021-04-04 01:10:58 +0800

    第七章7.9完成

--- lists/models.py
+++ lists/models.py
@@ -1,5 +1,13 @@
-from django.db import models
+from djongo import models
+
+
+class List(models.Model):
+    pass
 
 
 class Item(models.Model):
+    n_list = models.ForeignKey(
+        List,
+        on_delete=models.CASCADE,
+        null=True)
     text = models.TextField(default='')
